caebb78dcdb900e4d4e0bf86760c43372d242a73
tvo28-design <tvo28@ssis.edu.vn>
2026-05-20 09:14:44 +0700

    Update picture.py

--- picture.py
+++ picture.py
@@ -20,17 +20,16 @@ def draw_picture(width, height):
     # ==================================================================
     
     # code for RED triangles
-    sg.set_fill_color(colors[0]) # set fill color to red
     
-    for i in range(5):   # fill_triangle(x1, y1, x2, y2, x3, y3)
-        sg.fill_triangle(2*TRIANGLE_WIDTH, i*TRIANGLE_HEIGHT, 3*TRIANGLE_WIDTH, i*TRIANGLE_HEIGHT, 3*TRIANGLE_WIDTH, (i+1)*TRIANGLE_HEIGHT) 
-        
     
     # code for BLUE triangles
     
     
     # code for YELLOW triangles
-   
+   sg.set_fill_color(colors[5])
+    
+    for i in range(5):
+        sg.fill_triangle(1*TRIANGLE_WIDTH, i*TRIANGLE_HEIGHT, 2*TRIANGLE_WIDTH, i*TRIANGLE_HEIGHT, 2*TRIANGLE_WIDTH, (i+1)*TRIANGLE_HEIGHT)
    
     # code for GREEN triangles
  
